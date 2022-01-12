23c0369e97d5b88e608404760523996fd747f0f8
hzzzzzzzq <1351968213@qq.com>
2022-01-12 15:09:17 +0800

    feat: search components finished & test pass

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
-import './App.css';
+import "./App.css";
 
-import SearchBox from './components/SearchBox/searchBox';
+import SearchBox from "./components/SearchBox/searchBox";
 
 function App() {
+  const submit = (data) => {
+    const obj = {};
+    Object.entries(data).forEach(([k, v]) => {
+      obj[k] = v;
+    });
+    console.log(obj);
+  };
   const searchList = [
-    { key: 'input', label: 'input', type: 'INPUT', placeholder: '请输入姓名' },
+    { key: "input", label: "input", type: "INPUT", placeholder: "请输入姓名" },
     {
-      key: 'select',
-      label: 'select',
-      type: 'SELECT',
-      options: [{ id: 1, name: '选择1' }],
+      key: "select",
+      label: "select",
+      type: "SELECT",
+      options: [{ id: 1, name: "选择1" }],
     },
     {
-      key: 'checkBox',
-      label: 'chechBox',
-      type: 'CHECKBOX',
+      key: "checkBox",
+      label: "chechBox",
+      type: "CHECKBOX",
     },
-    { key: 'RANGEPICKER', name: 'RANGEPICKER', type: 'RANGEPICKER' },
+    { key: "switch", label: "switch", type: "SWITCH" },
+    {
+      key: "firstTime,secondTime",
+      label: "RangeTime",
+      type: "RANGEDATE",
+    },
+    { key: "start,end", label: "RangeTime2", type: "RANGEDATE" },
+    { key: "time", label: "Time", type: "DATE" },
+    { key: "month", label: "Month", type: "MONTH" },
   ];
   const fields = {
-    input: '1',
+    input: "1",
     select: 1,
     checkBox: true,
+    firstTime: "2021-12-31",
+    secondTime: "2022-12-20",
+    month: "2022-11-10",
+    time: "2021-10-10",
   };
   return (
     <div className="app">
-      <SearchBox searchList={searchList} fields={fields}></SearchBox>
+      <SearchBox
+        searchList={searchList}
+        fields={fields}
+        searchSubmit={submit}
+      ></SearchBox>
     </div>
   );
 }
--- src/components/SearchBox/searchBox.js
+++ src/components/SearchBox/searchBox.js
@@ -1,4 +1,14 @@
-import { Input, Select, Form, Checkbox, DatePicker, Button } from 'antd';
+import React, { useEffect } from "react";
+import {
+  Input,
+  Select,
+  Form,
+  Checkbox,
+  DatePicker,
+  Switch,
+  Button,
+} from "antd";
+import moment from "moment";
 
 const { Item } = Form;
 const { RangePicker, MonthPicker } = DatePicker;
@@ -7,16 +17,54 @@ const { Option } = Select;
 const SearchBox = (props) => {
   const [form] = Form.useForm();
 
+  useEffect(() => {
+    getInitailValue();
+  });
   const getInitailValue = () => {
-    const { fields } = props;
-
-    return fields;
+    // 默认数据处理
+    const { fields, searchList } = props;
+    if (fields) {
+      searchList.forEach((item) => {
+        switch (item.type) {
+          case "DATE":
+            form.setFieldsValue({ [item.key]: moment(fields[item.key]) });
+            break;
+          case "RANGEDATE":
+            if (item.key.includes(",")) {
+              const keyArr = item.key.split(",");
+              if (fields[keyArr[0]] && fields[keyArr[1]]) {
+                form.setFieldsValue({
+                  [item.key]: [
+                    moment(fields[keyArr[0]]),
+                    moment(fields[keyArr[1]]),
+                  ],
+                });
+              }
+            }
+            break;
+          case "MONTH":
+            if (fields[item.key]) {
+              form.setFieldsValue({
+                [item.key]: moment(fields[item.key], "YYYY-MM"),
+              });
+            }
+            break;
+          default:
+            form.setFieldsValue({
+              [item.key]: fields[item.key],
+            });
+            break;
+        }
+      });
+    }
   };
   const onFinish = (values) => {
-    console.log(values, 'submit');
+    // 提交
+    props.searchSubmit(values);
   };
 
   const onReset = () => {
+    // 重置
     if (props.onReset) {
       props.onReset();
     } else {
@@ -28,17 +76,31 @@ const SearchBox = (props) => {
     const itemList = [];
     if (searchList && searchList.length > 0) {
       searchList.forEach((item) => {
-        const { label, key, placeholder, options = [], width = '120px' } = item;
+        const { label, key, placeholder, options = [], width = "120px" } = item;
         let itemObj = null;
         switch (item.type) {
-          case 'INPUT':
+          /**
+           * name: 传入的 key 值，就是最后你传出时的值
+           * label: 各个组件前面的名称
+           * key: 给每一个 child 一个 key
+           * placeholder: 输入提醒
+           * options: select 选择项
+           *    item: {
+           *      id: '1', 传入/传出的值
+           *      name: '选择器', 显示的值
+           *    }
+           *
+           */
+          case "INPUT":
+            // 输入框
             itemObj = (
               <Item label={label} name={key} key={key}>
                 <Input placeholder={placeholder} style={{ width }} />
               </Item>
             );
             break;
-          case 'SELECT':
+          case "SELECT":
+            // 选择列表
             itemObj = (
               <Item label={label} name={key} key={key}>
                 <Select placeholder={placeholder} style={{ width }}>
@@ -56,19 +118,47 @@ const SearchBox = (props) => {
             );
 
             break;
-          case 'CHECKBOX':
+          case "CHECKBOX":
+            // 复选框
             itemObj = (
-              <Item name={key} valuePropName="checked" key={key}>
+              <Item name={key} key={key} valuePropName="checked">
                 <Checkbox>{label}</Checkbox>
               </Item>
             );
             break;
-          case 'RANGEPICKER':
+          case "SWITCH":
+            // 开关
+            itemObj = (
+              <Item label={label} name={key} key={key} valuePropName="checked">
+                <Switch />
+              </Item>
+            );
+            break;
+          case "DATE":
+            // 时间选择
+            itemObj = (
+              <Item label={label} name={key} key={key}>
+                <DatePicker />
+              </Item>
+            );
+            break;
+          case "RANGEDATE":
+            // 时间范围选择
             itemObj = (
               <Item name={key} label={label} key={key}>
                 <RangePicker />
               </Item>
             );
+            break;
+          case "MONTH":
+            // 月份选择
+            itemObj = (
+              <Item name={key} label={label} key={key}>
+                <MonthPicker />
+              </Item>
+            );
+            break;
+
           default:
         }
         itemList.push(itemObj);
@@ -77,13 +167,7 @@ const SearchBox = (props) => {
     return itemList;
   };
   return (
-    <Form
-      form={form}
-      layout="inline"
-      name="control-hooks"
-      onFinish={onFinish}
-      initialValues={getInitailValue()}
-    >
+    <Form form={form} layout="inline" name="control-hooks" onFinish={onFinish}>
       {initFormList()}
       <Item>
         <Button type="primary" htmlType="submit">
